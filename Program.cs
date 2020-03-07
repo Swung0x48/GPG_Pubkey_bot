4965a1f3cd9aaf9c1f95a2ded6cef967d17ebef7
Swung0x48 <swung0x48@outlook.com>
2020-03-07 23:30:23 +0800

    Test signing

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using GPG_Pubkey_bot.Misc;
+using Newtonsoft.Json;
+using Telegram.Bot;
+
+
+namespace GPG_Pubkey_bot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string APIKey = "1092977013:AAG_wU89sLH0dz4QbG3BuNxBwVUPl6yI0gg";
+            Vars.BotClient = new TelegramBotClient(APIKey);
+            
+            var bot = Vars.BotClient.GetMeAsync().Result;
+            Console.WriteLine(
+                $"ID: {bot.Id} \nName: {bot.FirstName}."
+            );
+        }
+    }
+}
