09f9af1dc0bea5d93e8cb83f8c45567663a2f556
Swung0x48 <swung0x48@outlook.com>
2020-03-08 11:25:53 +0800

    Conf file IO (TODO: Test it)

--- IO/File.cs
+++ IO/File.cs
@@ -1,7 +1,28 @@
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using GPG_Pubkey_bot.Utils;
+
 namespace GPG_Pubkey_bot.IO
 {
     public class File
     {
-        
+        public async Task<int> WriteConf(string Filename, object ConfObj)
+        {
+            await System.IO.File.WriteAllTextAsync(
+                Filename, 
+                await Json.Serialize(ConfObj)
+            );
+            return 0;
+        }
+
+        public async Task<object> ReadConf(string Filename)
+        {
+            string content = await System.IO.File.ReadAllTextAsync(
+                Filename,
+                Encoding.UTF8
+            );
+            return await Json.Parse<object>(content);
+        }
     }
 }
