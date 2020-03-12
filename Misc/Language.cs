2e015aca5e46a12fa20b72163a87b361dbf8f1ce
Swung0x48 <swung0x48@outlook.com>
2020-03-12 13:18:51 +0800

    Small fixes

---
+++ Misc/Language.cs
@@ -0,0 +1,11 @@
+
+
+namespace ChannelPosting_bot.Misc
+{
+    public static class Language
+    {
+        //public static string Message_RequireApiKey = "Please enter your Telegram bot API key:";
+        //public static string Message_ConfigNotFound = "Configuration file not found. A new one will be created.";
+        
+    }
+}
